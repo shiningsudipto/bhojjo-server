79e8b19e2a917e9ab028091d9a932d22cc034f31
Sudipta Das <shiningsudipto@gmail.com>
2024-11-29 20:34:05 +0600

    sub category functionality added

--- src/app/modules/category/category.service.ts
+++ src/app/modules/category/category.service.ts
@@ -1,5 +1,5 @@
-import { TCategory } from './category.interface'
-import { Category } from './category.model'
+import { TCategory, TSubCategory } from './category.interface'
+import { Category, SubCategory } from './category.model'
 
 const createCategoryIntoDB = async (payload: TCategory) => {
   const result = await Category.create(payload)
@@ -24,9 +24,49 @@ const deleteCategoryByIdFromDB = async (id: string) => {
   return result
 }
 
+// sub category
+
+const createSubCategoryIntoDB = async (payload: TSubCategory) => {
+  const isCategoryAvailable = await Category.findById(payload.category)
+  if (!isCategoryAvailable) {
+    throw new Error('Category not found!')
+  }
+  const result = await SubCategory.create(payload)
+  return result
+}
+
+const getSubCategoryFromDB = async () => {
+  const result = await SubCategory.find()
+  return result
+}
+
+const getSubCategoryByCategoryFromDB = async (id: string) => {
+  const result = await SubCategory.find({ category: id })
+  return result
+}
+
+const updateSubCategoryIntoDB = async (id: string, payload: TSubCategory) => {
+  const result = await SubCategory.findByIdAndUpdate(id, payload, {
+    new: true,
+    runValidators: true,
+  })
+  return result
+}
+
+const deleteSubCategoryFromDB = async (id: string) => {
+  const result = await SubCategory.findByIdAndDelete(id)
+  return result
+}
+
 export const categoryServices = {
   createCategoryIntoDB,
   updateCategoryIntoDB,
   getAllCategoryFromDB,
   deleteCategoryByIdFromDB,
+  // sub category
+  createSubCategoryIntoDB,
+  getSubCategoryFromDB,
+  getSubCategoryByCategoryFromDB,
+  updateSubCategoryIntoDB,
+  deleteSubCategoryFromDB,
 }
