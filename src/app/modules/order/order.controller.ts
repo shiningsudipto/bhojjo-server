881acad2a6236f96e06590f3551a39681a599459
Sudipta Das <shiningsudipto@gmail.com>
2024-12-01 00:55:56 +0600

    minor changes

--- src/app/modules/order/order.controller.ts
+++ src/app/modules/order/order.controller.ts
@@ -4,19 +4,15 @@ import sendResponse from '../../utils/sendResponse'
 import { orderServices } from './order.service'
 
 const createOrder = catchAsync(async (req, res) => {
-  //   const { order, orderItems } = req.body
+  const { order, orderItems } = req.body
 
-  const payload = req.body
-
-  console.log({ payload })
-
-  //   const result = await orderServices.createOrderIntoDB(order, orderItems)
+  const result = await orderServices.createOrderIntoDB(order, orderItems)
 
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
     message: 'Order created successfully',
-    data: payload,
+    data: result,
   })
 })
 
--- src/app/modules/order/order.routes.ts
+++ src/app/modules/order/order.routes.ts
@@ -1,9 +1,8 @@
 import express from 'express'
 import { orderControllers } from './order.controller'
-import { parseBody } from '../../middlewares/bodyParser'
 
 const router = express.Router()
 // order
-router.post('/', parseBody, orderControllers.createOrder)
+router.post('/', orderControllers.createOrder)
 
 export const OrderRoutes = router
